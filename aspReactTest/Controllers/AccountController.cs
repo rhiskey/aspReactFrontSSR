e101d44421945db2c51006ae00a1537250af603a
Vladimir Kiselev <first3395@gmail.com>
2021-03-16 02:13:05 +0300

    added hash function to secure password

--- aspReactTest/Controllers/AccountController.cs
+++ aspReactTest/Controllers/AccountController.cs
@@ -6,6 +6,8 @@ using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using aspReactTest.Models;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 
 namespace aspReactTest.Controllers
 {
@@ -22,8 +24,23 @@ namespace aspReactTest.Controllers
         [HttpPost("api/auth/signup")]
         public IActionResult Register(string username, string password, string email)
         {
-            //save user in db
-
+			// generate a 128-bit salt using a secure PRNG
+			byte[] salt = new byte[128 / 8];
+			using (var rng = RandomNumberGenerator.Create())
+			{
+				rng.GetBytes(salt);
+			}
+			Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+	 
+			// derive a 256-bit subkey (use HMACSHA1 with 10,000 iterations)
+			string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+				password: password,
+				salt: salt,
+				prf: KeyDerivationPrf.HMACSHA1,
+				iterationCount: 10000,
+				numBytesRequested: 256 / 8));
+				
+			// store username, email, hashed in DB
 
             var response = new
             {
